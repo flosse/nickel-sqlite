1ffe08f92d7611dd7553c2881370abb58f6d2718
Markus Kohlhase <mail@markus-kohlhase.de>
2016-02-04 10:26:01 +0100

    relicense under dual MIT/Apache-2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-// Copyright (c) 2015 Markus Kohlhase <mail@markus-kohlhase.de>
+// Copyright (c) 2015 - 2016 nickel-sqlite developers
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
 
 //! [nickel-sqlite](https://github.com/flosse/nickel-sqlite) is a
 //! [SQLite](http://www.sqlite.org/) middleware for
